0d09519ee3e8e0d6be458f4ab4c562c585ec8197
osidekyle <hawkinskyle67@gmail.com>
2020-06-22 22:25:18 -0700

    populated options with all currency symbols

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-var currencies=document.getElementById("currencies");
+var currenciesFrom=document.getElementById("currenciesFrom");
+var currenciesTo=document.getElementById("currenciesTo");
 
 
-window.addEventListener("load",loadApi);
+window.addEventListener("load",loadApi());
 
 
 
 
 function loadApi(){
     fetch("http://data.fixer.io/api/latest?access_key=25b5ed181f311e4488751ff1beffadac&format=1")
-    .then((res)=>console.log(res))
+    .then((res)=>res.json())
+    .then((res)=>
+      Object.keys(res.rates))
+    .then((res)=>{
+        let output="";
+        res.forEach(symbol=>
+        output+=`<option value="${symbol}">${symbol}</option>`)
+        currenciesFrom.innerHTML=output; 
+        currenciesTo.innerHTML=output; 
+    })
 }
