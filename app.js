3dc6029e10e3654e0328848e0bb4d29c44701d06
osidekyle <hawkinskyle67@gmail.com>
2020-06-22 23:01:49 -0700

    did all the mathy stuff

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var currenciesFrom=document.getElementById("currenciesFrom");
 var currenciesTo=document.getElementById("currenciesTo");
+var calcButton = document.getElementById("calcButton");
+var output = document.getElementById("output");
+var number = document.getElementById("number");
 
-
-window.addEventListener("load",loadApi());
-
+window.addEventListener("load",loadApi);
+calcButton.addEventListener("click",calculate);
 
 
 
@@ -18,5 +20,24 @@ function loadApi(){
         output+=`<option value="${symbol}">${symbol}</option>`)
         currenciesFrom.innerHTML=output; 
         currenciesTo.innerHTML=output; 
+    })
+}
+
+
+
+
+
+
+
+
+function calculate(){
+    fetch("http://data.fixer.io/api/latest?access_key=25b5ed181f311e4488751ff1beffadac&format=1")
+    .then((res)=>res.json())
+    .then((res)=>res.rates)
+    .then((res)=>{
+        let convertFrom=res[currenciesFrom.value];
+        let convertTo = res[currenciesTo.value];
+        let conversion=convertTo/convertFrom;
+        output.innerHTML = number.value*conversion;
     })
 }
