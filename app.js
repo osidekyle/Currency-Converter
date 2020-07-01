5f7f49a4e9dc691b5e6e2b0936f8e3f90708daca
osidekyle <hawkinskyle67@gmail.com>
2020-07-01 00:37:55 -0700

    added jquery

--- app.js
+++ app.js
@@ -1,45 +1,26 @@
-var currenciesFrom=document.getElementById("currenciesFrom");
-var currenciesTo=document.getElementById("currenciesTo");
-var calcButton = document.getElementById("calcButton");
-var output = document.getElementById("output");
-var number = document.getElementById("number");
-
-window.addEventListener("load",loadApi);
-calcButton.addEventListener("click",calculate);
-
+$(window).on("load",loadApi);
+$("#calcButton").on("click",calculate);
 
 
 function loadApi(){
-    fetch("http://data.fixer.io/api/latest?access_key=25b5ed181f311e4488751ff1beffadac&format=1")
-    .then((res)=>res.json())
+    $.getJSON("http://data.fixer.io/api/latest?access_key=25b5ed181f311e4488751ff1beffadac&format=1")
     .then((res)=>
       Object.keys(res.rates))
     .then((res)=>{
+        data=res;
         let output="";
         res.forEach(symbol=>
-        output+=`<option value="${symbol}">${symbol}</option>`)
-        currenciesFrom.innerHTML=output; 
-        currenciesTo.innerHTML=output; 
+        output+=`<option value="${symbol}">${symbol}</option>`);
+        $("#currenciesFrom").html(output); 
+        $("#currenciesTo").html(output);   
     })
 }
 
-
-
-
-
-
-
-
 function calculate(){
-    fetch("http://data.fixer.io/api/latest?access_key=25b5ed181f311e4488751ff1beffadac&format=1")
-    .then((res)=>res.json())
-    .then((res)=>res.rates)
-    .then((res)=>{
-        let convertFrom=res[currenciesFrom.value];
-        let convertTo = res[currenciesTo.value];
+        let convertFrom=data[$("#currenciesFrom").val()];
+        let convertTo = data[$("#currenciesTo").val()];
         let conversion=convertTo/convertFrom;
-        output.innerHTML = `<p id="res" class="mt-5">${number.value} in ${currenciesFrom.value}
-                            is ${Math.round((number.value*conversion)*100)/100}
-                            in ${currenciesTo.value}</p>`
-    })
+        $("#output").hide().html(`<p id="res" class="mt-5">${$("#number").val()} in ${currenciesFrom.value}
+                            is ${Math.round(($("#number").val()*conversion)*100)/100}
+                            in ${currenciesTo.value}</p>`).fadeIn(1000);  
 }
